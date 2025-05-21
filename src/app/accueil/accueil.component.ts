8c4eeeb7c6c38ce1d30f5205267b7d462ab23c8f
Garnier <pascalscn@hotmail.com>
2025-05-21 10:53:00 +0200

    1.0.0: renseignement des balises meta et titel

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { VersionService } from '../version.service';
 
 @Component({
@@ -10,11 +11,17 @@ export class AccueilComponent {
   versionService: VersionService;
   buildDate!: string;
   
-  constructor(versionService: VersionService) {
+  constructor(private meta: Meta, private title: Title,versionService: VersionService) {
     this.versionService = versionService;
   }
 
   ngOnInit() {
+     this.title.setTitle('FFCalculator');
+      this.meta.addTags([
+    { name: 'description', content: 'Page d’accueil de l’application Angular' },
+    { name: 'keywords', content: 'FFCalculator, Calcul de points FFC, Open 1, Open 2, Open 3, Access, Elite, U17, U23, Android, cyclisme, course FFC' },
+    { name: 'author', content: 'Pascal GARNIER' }
+  ]);
   this.versionService.getVersion().subscribe(data => {
     this.buildDate = data.buildDate;
   });
