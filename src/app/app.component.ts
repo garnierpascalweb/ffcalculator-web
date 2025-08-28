8d9502f8080e3aafa4251cd4ebfc21114ce37ce5
Garnier <pascalscn@hotmail.com>
2025-08-28 11:21:24 +0200

    1.0.0: AppComponent: suppression des balises title et meta, plutot dans index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -9,14 +8,8 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit { 
-  constructor(private meta: Meta, private title: Title,private http: HttpClient){}
-  ngOnInit(): void {
-    this.title.setTitle('FFCalculator');
-      this.meta.addTags([
-    { name: 'description', content: 'Page d’accueil de l’application Angular' },
-    { name: 'keywords', content: 'FFCalculator, Calcul de points FFC, Open 1, Open 2, Open 3, Access, Elite, U17, U23, Android, cyclisme, course FFC' },
-    { name: 'author', content: 'Pascal GARNIER' }
-  ]);
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {    
      //this.http.get(environment.trackingUrl).subscribe();
      // 28/08/2025 - POST et utilisation du nouveau service
     this.http.post(environment.trackingUrl,{}, {
