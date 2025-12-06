44303eeabc844ad2cbd1917d919c99fab10f5254
Garnier <pascalscn@hotmail.com>
2025-12-06 20:10:48 +0100

    1.0.0: simulator: assets/icons/logo : branchement des logos sur la bonne valeur

--- src/app/simulator/result-details/result-details.component.ts
+++ src/app/simulator/result-details/result-details.component.ts
@@ -15,31 +15,73 @@ export class ResultDetailsComponent {
   @Output() edit = new EventEmitter<void>();
   @Output() delete = new EventEmitter<void>();
 
-  constructor(private gridService : GridService){
+  constructor(private gridService: GridService) {
 
   }
 
- ngOnInit() {
-  //console.log("LOGO =", this.place);
-    console.log('result reçu =', this.result);
-  console.log('type =', typeof this.result);
-}
+  ngOnInit() {
 
-ngOnChanges(changes: SimpleChanges) {
-  console.log('result reçu :', this.result);
-}
-  getLogo(code: string):string {
-    let gridLogo = "rien";
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+  }
+
+  getAvatarUrl(): string {
+    return `assets/icons/logo/${this.getLogo(this.result?.code)}`;
+    //return `assets/icons/logo/logo-elite.svg`;
+  }
+
+  getLogo(code: string): string { 
+    let svgLogo: string = 'logo-default.svg';
     this.gridService.getGridByCode(code).subscribe(grid => {
-      if (grid){
-        gridLogo = grid.logo;
+      if (grid) {        
+        switch (grid.logo) {
+          case 'Elite':
+          case 'CDF N1':
+          case 'CDF N2':
+          case 'CDF N3':
+            svgLogo = 'logo-elite.svg';
+            break;
+
+          case 'Open 1/2':
+            svgLogo = 'logo-open-1-2.svg';
+            break;
+
+          case 'Open 1/2/3':
+            svgLogo = 'logo-open-1-2-3.svg';
+            break;
+
+          case 'Open 2/3':
+            svgLogo = 'logo-open-2-3.svg';
+            break;
+
+          case 'Open 3':
+            svgLogo = 'logo-open-3.svg';
+            break;
+
+          case 'U23':
+            svgLogo = 'logo-u23.svg';
+            break;
+
+          case 'U19':
+            svgLogo = 'logo-u19.svg';
+            break;
+
+          case 'U17':
+            svgLogo = 'logo-u17.svg';
+            break;
+
+          default:
+            svgLogo = 'logo-default.svg'; // valeur par défaut si aucun match
+        }
+
       } else {
-        
+        svgLogo = 'logo-default.svg'; // valeur par défaut si aucun match
       }
     }
 
     );
-    return gridLogo;
-    
+    return svgLogo;
   }
 }
