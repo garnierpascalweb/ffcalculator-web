2cae1dee72d4e826fc612480e48fdae043c439aa
Garnier <pascalscn@hotmail.com>
2025-12-26 23:10:35 +0100

    1.0.0: overwiew: affichage furtif des points

--- src/app/simulator/overview/overview.component.ts
+++ src/app/simulator/overview/overview.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RankingService } from 'src/app/services/ranking.service';
 import { LoggerService } from 'src/app/services/logger.service';
 import { ResultService } from 'src/app/services/result.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
 
+    pts: number;
+  ranking$: Observable<number>;
   constructor(private log: LoggerService, private resultService: ResultService, private rankingService : RankingService){
 
+  }
+  ngOnInit(): void {
+    this.pts = 5.64;
+    this.ranking$ = this.getRanking(this.pts);
+  }
+
+
+
+
+  getRanking(pts: number){
+    return this.rankingService.getRanking(pts);
   }
 }
