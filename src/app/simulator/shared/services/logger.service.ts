9650289887664ff9efe7ab2133317d3462c1895d
Garnier <pascalscn@hotmail.com>
2026-03-07 09:08:33 +0100

    1.0.0: logs: desactivation du logging en prod

--- src/app/simulator/shared/services/logger.service.ts
+++ src/app/simulator/shared/services/logger.service.ts
@@ -1,27 +1,35 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggerService {
 
+  enabled: boolean;
+
   constructor() {
-
+    this.enabled = !environment.production;
+    console.log(' loggine enabled : ' + this.enabled);
   }
 
-  trace(className: string, message: string){
-    console.log("[TRACE] [" + className + "] " + message);
+  trace(className: string, message: string) {
+    if (this.enabled)
+      console.log("[TRACE] [" + className + "] " + message);
   }
 
-  debug(className: string, message: string){
-    console.log("[DEBUG] [" + className + "] " + message);
+  debug(className: string, message: string) {
+    if (this.enabled)
+      console.log("[DEBUG] [" + className + "] " + message);
   }
 
-  info(className: string, message: string){
-    console.log("[INFO] [" + className + "] " + message);
+  info(className: string, message: string) {
+    if (this.enabled)
+      console.log("[INFO] [" + className + "] " + message);
   }
 
-  error(className: string, message: string){
-    console.log("[ERROR] [" + className + "] " + message);
+  error(className: string, message: string) {
+    if (this.enabled)
+      console.log("[ERROR] [" + className + "] " + message);
   }
 }
