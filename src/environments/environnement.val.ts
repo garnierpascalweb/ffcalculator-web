492f4da1af1f67263c208b51fbc20eac6eec928d
Garnier <pascalscn@hotmail.com>
2026-03-10 10:46:12 +0100

    1.0.0: mise en place d'une configuration de validation

---
+++ src/environments/environnement.val.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  trackingUrl: 'https://garnierpascalweb.fr/core/api/VisiteursApi.php',
+  addResultUrl: 'https://garnierpascalweb.fr/app/ffcalculator/api/FFCPointsApi.php',
+  env: 'dev'
+};
